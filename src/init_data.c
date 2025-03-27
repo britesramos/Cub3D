ac3ff9c9a4aad3547b6cdd58dbe1eb4caecfb356
sramos <sramos@student.codam.nl>
2025-03-27 10:34:48 +0100

    norminette fixed

--- src/init_data.c
+++ src/init_data.c
@@ -14,7 +14,8 @@
 
 void	init_player(t_data *input)
 {
-	input->player->pos_x = (input->player_sp_x * TILE_SIZE) + TILE_SIZE / 2; //divided by 2 so that the player is in the middle of a tile
+	input->player->pos_x = (input->player_sp_x * TILE_SIZE) + TILE_SIZE / 2;
+	//divided by 2 so that the player is in the middle of a tile
 	input->player->pos_y = (input->player_sp_y * TILE_SIZE) + TILE_SIZE / 2;
 	input->player->fov_rad = (60 * PI) / 180; //formula for degrees to radians
 	input->player->angle = facing_angle(input->player_facing);
@@ -40,23 +41,14 @@ static void	init_parse_utils(t_data *data)
 	data->parse_utils->f_util = NULL;
 }
 
-void	init_data(t_data *data)
+void	init_data_helper(t_data *data)
 {
-	data->map = NULL;
-	data->no = NULL;
-	data->so = NULL;
-	data->we = NULL;
-	data->ea = NULL;
-	data->map_height = 0;
-	data->map_width = 0;
 	data->f_color = ft_calloc(sizeof(char *), 3 + 1);
 	if (!data->f_color)
 		error_print_exit(data, "Fail alloc f_color\n", -2);
 	data->c_color = ft_calloc(sizeof(char *), 3 + 1);
 	if (!data->c_color)
 		error_print_exit(data, "Fail alloc c_color\n", -2);
-	data->player_sp_x = 0;
-	data->player_sp_y = 0;
 	data->player_facing = ft_calloc(sizeof(char), 1 + 1);
 	if (!data->player_facing)
 		error_print_exit(data, "Fail alloc player_facing\n", -2);
@@ -72,5 +64,19 @@ void	init_data(t_data *data)
 	data->ray = ft_calloc(1, sizeof(t_ray));
 	if (!data->ray)
 		error_print_exit(data, "Fail alloc ray struct", -2);
+}
+
+void	init_data(t_data *data)
+{
+	data->map = NULL;
+	data->no = NULL;
+	data->so = NULL;
+	data->we = NULL;
+	data->ea = NULL;
+	data->map_height = 0;
+	data->map_width = 0;
+	data->player_sp_x = 0;
+	data->player_sp_y = 0;
+	init_data_helper(data);
 	init_parse_utils(data);
 }

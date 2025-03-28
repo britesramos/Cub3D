658efef91ade7dcb667a6653a591a8c89cc4367e
sramos <sramos@student.codam.nl>
2025-03-28 12:10:19 +0100

    Fixed norminette

--- src/execution/mini_map.c
+++ src/execution/mini_map.c
@@ -6,7 +6,7 @@
 /*   By: sramos <sramos@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/20 18:01:50 by sramos        #+#    #+#                 */
-/*   Updated: 2025/03/28 12:02:34 by sramos        ########   odam.nl         */
+/*   Updated: 2025/03/28 12:09:17 by sramos        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,37 +17,42 @@ uint32_t	ft_my_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t a)
 	return (r << 24 | g << 16 | b << 8 | a);
 }
 
+static void	draw_tile(t_data *data, uint32_t x, uint32_t y)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < MM_TILE_SIZE)
+	{
+		while (j < MM_TILE_SIZE)
+		{
+			if (data->map[y][x] == '0')
+				mlx_put_pixel(data->img, (x * MM_TILE_SIZE) + j,
+					(y * MM_TILE_SIZE) + i, data->mini_map->colour[1]);
+			else if (data->map[y][x] == '1')
+				mlx_put_pixel(data->img, (x * MM_TILE_SIZE) + j,
+					(y * MM_TILE_SIZE) + i, data->mini_map->colour[0]);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+}
+
 static void	draw_map(t_data *data)
 {
-	int				i;
-	int				j;
 	uint32_t		x;
 	uint32_t		y;
 
 	x = 0;
 	y = 0;
-	i = 0;
-	j = 0;
 	while (data->map[y])
 	{
 		while (data->map[y][x])
 		{
-			while (i < MM_TILE_SIZE)
-			{
-				while (j < MM_TILE_SIZE)
-				{
-					if (data->map[y][x] == '0')
-						mlx_put_pixel(data->img, (x * MM_TILE_SIZE) + j,
-							(y * MM_TILE_SIZE) + i, data->mini_map->colour[1]);
-					else if (data->map[y][x] == '1')
-						mlx_put_pixel(data->img, (x * MM_TILE_SIZE) + j,
-							(y * MM_TILE_SIZE) + i, data->mini_map->colour[0]);
-					j++;
-				}
-				j = 0;
-				i++;
-			}
-			i = 0;
+			draw_tile(data, x, y);
 			x++;
 		}
 		x = 0;
